202aa49b9dd09f78b888207f9b544e8581e3c9ed
Khushi Rupera <khushirupera.nitsan@gmail.com>
2024-05-13 10:16:28 +0530

    [TASK]Update extension details

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,17 +2,19 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Open Street Map Plugin',
-    'description' => 'This extension can easily integrate of powerful features of OpenStreet map like find-places, find-direction between locations etc. Live-Demo: https://demo.t3planet.com/t3-extensions/openstreet-map You can download PRO version for more-features & free-support at https://t3planet.com/typo3-openstreetmap-extension',
+    'description' => 'The TYPO3 OpenStreetMap extension allows you to effortlessly embed maps from OpenStreetMap into your TYPO3 website. It enables features such as 5+ OpenStreetMap styles, displaying physical addresses, location popups, and 10+ customization options, compatible with TYPO3 v12.
+
+	*** Live Demo: https://demo.t3planet.com/t3-extensions/openstreet-map *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-openstreetmap-extension',
     'category' => 'plugin',
     'author' => 'T3: Jay Bhalgamiya, QA: Krishna Dhapa',
     'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'NITSAN Technologies Pvt Ltd',
+    'author_company' => 'T3Planet // NITSAN',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.1.0',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.0.0-11.5.99',
